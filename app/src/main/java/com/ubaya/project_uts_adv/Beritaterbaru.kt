5ffa034bb6a50ee75f4f935293f2ccbd34ba1598
H P <muhmmddani10@gmail.com>
2024-04-21 21:04:19 +0700

    uts

---
+++ app/src/main/java/com/ubaya/project_uts_adv/Beritaterbaru.kt
@@ -0,0 +1,11 @@
+package com.ubaya.project_uts_adv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Beritaterbaru : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_beritaterbaru)
+    }
+}
